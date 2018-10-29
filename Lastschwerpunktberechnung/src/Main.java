91a446e44a5018814a4ca8d9e37ee6139aeadc97
Timo.Nordhorn <Timo.Nordhorn@NOTEB68.emutec.local>
2018-10-29 17:05:27 +0100

    Edits in Main & Menu

--- Lastschwerpunktberechnung/src/Main.java
+++ Lastschwerpunktberechnung/src/Main.java
@@ -13,6 +13,11 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Menu menu = new Menu();
+		greetz();
+		menu.test();
+	}
+	private static void greetz() {
 		System.out.println("Test 1.");
 		System.out.println("Test 2");
 		System.out.println("Test 3");
--- Lastschwerpunktberechnung/src/Menu.java
+++ Lastschwerpunktberechnung/src/Menu.java
@@ -7,5 +7,8 @@
  *
  */
 public class Menu {
-
+	public void test() {
+		System.out.println("Test 4!");	
+	}
+	
 }
