62d15c0a229a8111bbf31e7f3169871a442a1ecb
Timo.Nordhorn <Timo.Nordhorn@emutec.de>
2018-12-05 13:41:05 +0100

    GUI changes
    The MainWindow should now be fully oriented in GridBoxLayout.
    Optimizing of EditBuilding and EditSector window have begun.

--- Lastschwerpunktberechnung/src/de/emutec/lastschwerpunkt/workwindow/MainWindow.java
+++ Lastschwerpunktberechnung/src/de/emutec/lastschwerpunkt/workwindow/MainWindow.java
@@ -8,8 +8,6 @@ public class MainWindow extends JFrame {
 
 	// Objects to use on this window
 	private JPanel panelNavigation = new JPanel();
-	private JPanel panelEditBuildings = new JPanel();
-	private JPanel panelEditSectors = new JPanel();
 	private JPanel panelMap = new JPanel();
 	private JPanel panelImage = new JPanel();
 
@@ -25,7 +23,7 @@ public class MainWindow extends JFrame {
 	private JMenuItem mntmHelp = new JMenuItem("?");
 
 	private JTree tree = new JTree();
-
+	private JScrollPane ScrollPane1 = new JScrollPane(tree);
 	private JButton btnGebudeHinzufgen = new JButton("Geb\u00E4ude hinzuf\u00FCgen");
 	private JButton btnGebudeLschen = new JButton("Geb\u00E4ude l\u00F6schen");
 	private JButton btnGebudeBearbeiten = new JButton("Geb\u00E4ude bearbeiten");
@@ -43,7 +41,6 @@ public class MainWindow extends JFrame {
 		this.setMinimumSize(new Dimension(800, 600));
 		this.setExtendedState(Frame.MAXIMIZED_BOTH);
 		this.setTitle("Lastschwerpunktberechnung");
-		this.setBounds(100, 100, 1550, 910);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// Set the main parts of the window
@@ -62,64 +59,65 @@ public class MainWindow extends JFrame {
 		menu.add(mntmExit);
 
 		// Left panel for tree and buttons
-		panelNavigation.setMinimumSize(new Dimension(50, 200));
-		panelNavigation.setPreferredSize(new Dimension(250, 0));
 		panelNavigation.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 
+		c.gridx = 0;
+		c.fill = GridBagConstraints.BOTH;
+		c.weighty = 1;
+	
+		panelNavigation.add(ScrollPane1, c);
 		tree.setDragEnabled(true);
 		tree.setRowHeight(25);
 		tree.setAutoscrolls(true);
-		tree.setMaximumSize(new Dimension(200, 0));
-		tree.setMinimumSize(new Dimension(100, 0));
 		tree.setShowsRootHandles(true);
 		tree.setRootVisible(false);
-
-		panelNavigation.add(tree, c);
-
-		// Panel for building-editing buttons
-		// panelEditBuildings.setPreferredSize(new Dimension(0, 100));
-		// panelEditBuildings.setMinimumSize(new Dimension(10, 150));
-		c.anchor = GridBagConstraints.SOUTH;
-		c.gridwidth = GridBagConstraints.REMAINDER;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0;
+		c.gridwidth = GridBagConstraints.REMAINDER;		
 
 		panelNavigation.add(btnGebudeHinzufgen, c);
 		panelNavigation.add(btnGebudeLschen, c);
 		panelNavigation.add(btnGebudeBearbeiten, c);
-
-		// panel for the sector-editing buttons
-		// panelEditSectors.setPreferredSize(new Dimension(0,100));
-		// panelEditSectors.setMinimumSize(new Dimension(10,150));
-		c.gridy = 2;
-		panelNavigation.add(panelEditSectors, c);
-		panelEditSectors.setLayout(new GridBagLayout());
-
-		c.gridy = 0;
-		panelEditSectors.add(btnAddSector, c);
-		c.gridy = 1;
-		panelEditSectors.add(btnEditSector, c);
-		c.gridy = 2;
-		panelEditSectors.add(btnDelSector, c);
-
+		panelNavigation.add(btnAddSector, c);
+		panelNavigation.add(btnEditSector, c);
+		panelNavigation.add(btnDelSector, c);
+		
 		// Panel for the Map and editing
 		panelMap.setLayout(new BorderLayout(5, 5));
 		panelMap.add(toolBar, BorderLayout.NORTH);
 		toolBar.setFloatable(false);
+		toolBar.setBackground(new Color(0, 123, 10));
+		toolBar.add(new JLabel("Toolbar"));
 
 		panelMap.add(panelImage, BorderLayout.CENTER);
-
+		panelImage.setBackground(new Color(53, 66, 132));
+		
+		this.pack();
 	} // End of constructor
 
-	public void btnGebudeHinzufgenListener(ActionListener listenForAddBuildingButton) {
+	public void btnAddBuildingListener(ActionListener listenForAddBuildingButton) {
 		btnGebudeHinzufgen.addActionListener(listenForAddBuildingButton);
 	}
 
-	public void btnGebudeBearbeitenListener(ActionListener listenForEditBuildingButton) {
+	public void btnEditBuildingListener(ActionListener listenForEditBuildingButton) {
 		btnGebudeBearbeiten.addActionListener(listenForEditBuildingButton);
 	}
 
-	public void btnGebudeEntfernenListener(ActionListener listenForDeleteBuildingButton) {
+	public void btnDelBuildingListener(ActionListener listenForDeleteBuildingButton) {
 		btnGebudeBearbeiten.addActionListener(listenForDeleteBuildingButton);
+	}
+	
+	public void btnAddSectorListener(ActionListener listenForAddSectorButton) {
+		btnAddSector.addActionListener(listenForAddSectorButton);
+	}
+
+	public void btnEditSectorListener(ActionListener listenForEditSectorButton) {
+		btnEditSector.addActionListener(listenForEditSectorButton);
+	}
+
+	public void btnDelSectorListener(ActionListener listenForDeleteSectorButton) {
+		btnDelSector.addActionListener(listenForDeleteSectorButton);
 	}
 
 }
